394c5bcb63948acea1b3300e22a0290120aeb2cd
BizualemTesfa <bizualemtesfa1@gmail.com>
2025-09-13 03:32:58 -0700

    header part changed to be responsive

--- src/components/header/Headder.jsx
+++ src/components/header/Headder.jsx
@@ -1,51 +1,57 @@
-import React from "react";
-import Netflix_Logo from "../../assets/images/logo.png"
+import React, { useState } from "react";
+import Netflix_Logo from "../../assets/images/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import "./Header.css"
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import "./Header.css";
 
-const Headder = () => {
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <div className="outer_div_container">
-        <div className="first_div">
-          <div className="header_center">
-            <img
-              src={Netflix_Logo}
-              alt="Netflix_Logo"
-              className="netflix_logo"
-            />
-            <ul className="nav_links">
-              <li>Home</li>
-              <li>Tv Shows</li>
-              <li>Movies</li>
-              <li>Latest</li>
-              <li>My List</li>
-              <li>Browse by LAnguge</li>
-            </ul>
-          </div>
-          <div className="header_right">
-            <ul className="nav_links">
-              <li>
-                <SearchIcon />
-              </li>
-              <li>
-                <NotificationsIcon />
-              </li>
-              <li>
-                <AssignmentIndIcon />
-              </li>
-              <li>
-                <ArrowDropDownIcon />
-              </li>
-            </ul>
-          </div>
+    <div className="outer_div_container">
+      <div className="first_div">
+        {/* Logo + Nav */}
+        <div className="header_center">
+          <img src={Netflix_Logo} alt="Netflix_Logo" className="netflix_logo" />
+          <ul className={`nav_links ${isOpen ? "active" : ""}`}>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Language</li>
+          </ul>
+        </div>
+
+        {/* Right icons */}
+        <div className="header_right">
+          <ul className="nav_links">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AssignmentIndIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+
+          {/* Hamburger menu */}
+          <button className="menu_btn" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <CloseIcon /> : <MenuIcon />}
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default Headder;
+export default Header;
